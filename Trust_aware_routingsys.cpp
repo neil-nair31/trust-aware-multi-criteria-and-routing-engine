9e3458014c5fb1f57f3bab3f7033d6db803c7b73
neil-nair31 <neilajithnair@gmail.com>
2026-02-13 02:30:38 +0530

    Add files via upload

--- Trust_aware_routingsys.cpp
+++ Trust_aware_routingsys.cpp
@@ -32,4 +32,39 @@ int computescore(Edge e){
     return e.latency + e.cost + trust_pen; 
 }
 
+int djikstra(int start, int end){
+    int dist[MAXN];
+    if(int i = 0; i < MAXN; i++){
+        dist[i] = INT_MAX;
+    }
+priority_queue<
+    pair<int, int>,
+    vector<pair<int, int>>,
+    greater<pair<int, int>>
+> pq;
 
+dist[start] = 0;
+pq.push({0, start});
+
+while(!pq.empty()){
+    int curr_cost = pq.top().first;
+    int node = pq.top().second;
+    pq.pop();
+
+    if(node == end){
+        return curr_cost;
+    }
+
+    for(int i = 0; i < edgeCount[node]; i++){
+        Edge e = adj[node][i]
+        if e.trust < trust_score {
+            continue; 
+        }
+        int new_cost = curr_cost + computescore(e);
+        if(new_cost < dist[e.to]){
+            dist[e.to] = new_cost;
+            pq.push({new_cost, e.to});
+        }
+    }
+}
+}
